320ffe06da5dd55aef67400eb6531200dc1b3cee
Ponlponl123 <55607166+ponlponl123@users.noreply.github.com>
2025-04-09 22:51:07 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ export const redisClient = new ioredis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
-    db: Number(process.env.REDIS_DB),
+    db: Number(process.env.REDIS_DB || 0),
     lazyConnect: true,
     enableReadyCheck: true,
     retryStrategy: (times) => {
