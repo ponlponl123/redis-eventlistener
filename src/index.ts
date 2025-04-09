4038865b093e7e5a600e1333ee7114391af4fc26
Ponlponl123 <55607166+ponlponl123@users.noreply.github.com>
2025-04-09 23:29:52 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,35 @@ import ioredis from 'ioredis';
 import mariadb from 'mariadb';
 import events from './events/index';
 
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = Number(process.env.DB_PORT);
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = Number(process.env.REDIS_PORT);
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const REDIS_DB = Number(process.env.REDIS_DB || 0);
+
+console.info(
+    '[Database Connection Info] -------------\n' +
+    `  DB_HOST: ${DB_HOST}\n` +
+    `  DB_USER: ${DB_USER}\n` +
+    `  DB_NAME: ${DB_NAME}\n` +
+    `  DB_PORT: ${DB_PORT}\n` +
+    '[Redis Connection Info] ----------------\n' +
+    `  REDIS_HOST: ${REDIS_HOST}\n` +
+    `  REDIS_PORT: ${REDIS_PORT}\n` +
+    `  REDIS_DB: ${REDIS_DB}\n` +
+    '----------------------------------------'
+)
+
 export const database = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
     connectionLimit: 5,
     acquireTimeout: 30000,
     connectTimeout: 30000,
@@ -15,10 +38,10 @@ export const database = mariadb.createPool({
 });
 
 export const redisClient = new ioredis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    password: process.env.REDIS_PASSWORD,
-    db: Number(process.env.REDIS_DB || 0),
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+    db: REDIS_DB,
     lazyConnect: true,
     enableReadyCheck: true,
     retryStrategy: (times) => {
